2373f0b9541e69653fa23b503fc0d4123d42fa39
iamgoeldhruv <goeldhruv9876@gmail.com>
2023-08-20 19:02:41 +0530

    login screen ui done

--- lib/UI/login-screen.dart
+++ lib/UI/login-screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:dash_insta/widgets/text-field-input.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: LoginScreen(),
+  ));
+}
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -26,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
       body: ListView(
         children: <Widget>[
           Container(
-            height: 400,
+            height: 300,
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/images/loginuilogo.avif'),
@@ -34,7 +40,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
           ),
-          const SizedBox(height: 64),
+          const SizedBox(height: 40),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
             child: TextFieldInput(
@@ -53,15 +59,58 @@ class _LoginScreenState extends State<LoginScreen> {
               isPass: true,
             ),
           ),
+          const SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: InkWell(
+              onTap: () {
+                // Add your login logic here
+              },
+              child: Container(
+                child: const Text(
+                  'Log in',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: const Color.fromARGB(255, 26, 81, 126),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 156),
+          Padding(
+            padding: const EdgeInsets.only(right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add navigation to your registration screen
+                  },
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 14),
+                    primary: Colors.blue,
+                    elevation: 0,
+                  ),
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
           // Add other widgets or controls as needed
         ],
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: LoginScreen(),
-  ));
 }
